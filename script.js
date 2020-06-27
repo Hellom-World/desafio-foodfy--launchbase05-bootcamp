8d2784f356ed18d2b81558a33126483aef0459c7
Hellom Oliveira <hellom-mendes@hotmail.com>
2020-06-28 00:04:29 +0100

    desafio-foodfy--launchbase05-bootcamp

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ const overlay = document.querySelector('.modal-overlay')
 for(let card of cards){
     card.addEventListener('click', function(){
         overlay.classList.add('active')
+
+    
+        const pegaimg = card.getAttribute('id')
+        const title = card.querySelector(".content").innerHTML;
+        const text = card.querySelector(".info").innerHTML;
+        
+        document.querySelector('.modalimg').src = (`assets/${pegaimg}.png`)
+        overlay.querySelector(".title").innerHTML = title;
+        overlay.querySelector(".info").innerHTML = text; 
+
+        
+
+       
     })
 }
 
