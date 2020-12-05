5ea969c7705a6ca3758485f1f26e9d118a13ebe7
Hellom Oliveira <hellom-mendes@hotmail.com>
2020-12-05 00:08:39 +0000

    Administração do foodfy em andamento

--- recipes.js
+++ recipes.js
@@ -3,3 +3,24 @@ const recData = require("./data")
 exports.index = function(req, res){
     return res.render('admin/index.njk', {itens: recData})
 }
+
+exports.show = function(req, res){
+    const { id } = req.params 
+
+    const foundRecipes = recData.find(function(recipe){
+        return id == recipe.index										
+    })
+
+    if (!foundRecipes) return res.send("recipe not found!")
+    
+    
+
+    const recipe = {
+        ...foundRecipes
+    }
+     
+
+    return res.render("admin/receita.njk", {recipe})
+
+
+}			
