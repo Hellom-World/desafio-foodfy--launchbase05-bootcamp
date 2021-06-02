628505ee70810d77705a4f2e1fa5ec979a73ae52
Hellom Oliveira <hellom-mendes@hotmail.com>
2021-06-02 21:18:20 +0100

    Persistindo dados do foodfy 2.2 - concluido

--- public/script.js
+++ public/script.js
@@ -120,4 +120,14 @@ escondeInfo.addEventListener('click', function(){
 
     const pages = paginate(page, total)
     console.log(pages)
-      
+    let elements = ""
+
+    for (let page of pages) {
+        if (String(page).includes("...")) {
+            elements += `<span>${page}</span>`
+        } else {
+            elements += `<a href= "?page=${page}">${page}</a>`
+        }    
+    }
+    
+    pagination.innerHTML = elements
